cd825d85f90cda590997e967596e5e26906a02ea
wuqianye <natsumi3613@gmail.com>
2021-01-09 17:36:51 -0500

    restoreAll done, close #10

--- script.js
+++ script.js
@@ -8,3 +8,17 @@ colorSelect.addEventListener("change", () => {
         item.style.background = color;
     });
 });
+
+//create a restore button
+const restore = document.createElement("button");
+restore.innerText = "Restore";
+restore.style.marginLeft = "10px";
+//restore grid-item to initial color when button is clicked
+restore.addEventListener("click", () => {
+    document.querySelectorAll(".grid-item").forEach(item => {
+        item.style.background = "rgb(255, 255, 255)";
+    });
+})
+//append the restore button
+document.querySelector("body").append(restore);
+document.querySelector("select").after(restore);
