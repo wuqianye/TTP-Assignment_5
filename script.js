94f0df54b366e39bc074f497604ba59795c4760e
wuqianye <natsumi3613@gmail.com>
2021-01-09 17:25:00 -0500

    fillAll done, close #9

--- script.js
+++ script.js
@@ -1,12 +1,10 @@
 //dropdown menu
 const colorSelect = document.getElementById("color");
 
-//get color value and change color of all uncolored grid-item
+//get color value and change color of all grid-item
 colorSelect.addEventListener("change", () => {
     const color = colorSelect.value;
     document.querySelectorAll(".grid-item").forEach(item => {
-        if (getComputedStyle(item).backgroundColor == "rgb(255, 255, 255)") {
-            item.style.background = color;
-        }
+        item.style.background = color;
     });
 });
