304e3b415bc1a37f1d4237bd0050a7cc31c5e75e
wuqianye <natsumi3613@gmail.com>
2021-01-09 17:06:57 -0500

    cilckChangeSingleColor done, close #7

--- script.js
+++ script.js
@@ -6,3 +6,11 @@ let color;
 colorSelect.addEventListener("change", () => {
     color = colorSelect.value;
 });
+
+//grid-item
+const gridItem = document.querySelectorAll(".grid-item");
+gridItem.forEach(itme => {
+    itme.addEventListener("click", (e) => {
+        e.target.style.background = color;
+    })
+});
