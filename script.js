d9514db8c71407085f8b69a0c9ce3a5bf8d0e8e6
wuqianye <natsumi3613@gmail.com>
2021-01-09 22:29:46 -0500

    clickHoldHover done, close #11

--- script.js
+++ script.js
@@ -6,3 +6,31 @@ let color;
 colorSelect.addEventListener("change", () => {
     color = colorSelect.value;
 });
+
+//grid-container
+const gridContainer = document.querySelector(".grid-container");
+
+//grid-item
+const gridItem = document.querySelectorAll(".grid-item");
+
+//change color
+let changeColor = (e) => {
+    e.target.style.background = color;
+}
+
+//add event listener
+gridContainer.addEventListener("mousedown", (e) => {
+    e.target.style.background = color;
+    //add mouseover event on grid-item
+    gridItem.forEach(item => {
+        item.addEventListener("mouseover", changeColor);
+    });
+});
+
+//remove event listener
+document.addEventListener("mouseup", () => {
+    //remove mouseover event on grid-item
+    gridItem.forEach(item => {
+        item.removeEventListener("mouseover", changeColor);
+    });
+});
